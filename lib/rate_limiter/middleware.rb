6f899899f864a5a56eec90eedbfa462c70318641
Lachlan Sylvester <lachlan.sylvester@hypothetical.com.au>
2018-07-19 23:13:14 +1000

    refactor setting of rate limit headings

--- lib/rate_limiter/middleware.rb
+++ lib/rate_limiter/middleware.rb
@@ -2,9 +2,10 @@ require 'rate_limiter/store'
 
 module RateLimiter
   class Middleware
-    def initialize(app, options)
+    def initialize(app, limit:, period:)
       @app = app
-      @options = options
+      @limit = limit
+      @period = period
       @store = Store.new
     end
 
@@ -15,18 +16,26 @@ module RateLimiter
 
       counter = Counter.new(@store, request.remote_ip)
       counter.incr
-      counter.expires_in ||= @options[:period]
+      counter.expires_in ||= @period
 
-      if counter.exceeds?(@options[:limit])
+      if counter.exceeds?(@limit)
         response = ActionDispatch::Response.new(427, {}, "Rate Limit Exceeded. Please retry in #{counter.expires_in} seconds.")
       else
         response = ActionDispatch::Response.new(*@app.call(env))
       end
-      response.headers["X-RateLimit-Limit"] = @options[:limit].to_s
-      response.headers["X-RateLimit-Remaining"] = (@options[:limit] - counter.value).to_s
-      response.headers["X-RateLimit-Reset"] = counter.expires_at.to_i.to_s
+      response.headers.merge!(rate_limit_response_headers(counter))
 
       response.to_a
+    end
+
+    protected
+
+    def rate_limit_response_headers(counter)
+      {
+        "X-RateLimit-Limit"     => @limit.to_s,
+        "X-RateLimit-Remaining" => (@limit - counter.value).to_s,
+        "X-RateLimit-Reset"     => counter.expires_at.to_i.to_s
+      }
     end
   end
 end
