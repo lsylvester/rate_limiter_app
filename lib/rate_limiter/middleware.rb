f53e6e64141e43c7bbae1c6f1b495c361830c747
Lachlan Sylvester <lachlan.sylvester@hypothetical.com.au>
2018-07-21 23:57:02 +1000

    add details about how to initialize the rate limiter middleware

--- lib/rate_limiter/middleware.rb
+++ lib/rate_limiter/middleware.rb
@@ -2,8 +2,13 @@ require 'rate_limiter/store'
 
 module RateLimiter
   class Middleware
-
-    
+    # This middleware provides rate limiting. It is configured with the following options:
+    #
+    # * `:limit`      - The number of requests to be allowed within the period
+    # * `:period`     - Number of seconds for the rate limited window.
+    # * `:identifier` - A `Proc` that accepts the `ActionDispatch::Request` and returns a unique string for the requester . By default it extracts the `remote_ip`
+    #
+    # For example usage, see the HomeController.
 
     def initialize(app, identifier: :remote_ip.to_proc, limit:, period:)
       @app = app
