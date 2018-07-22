394eaca1a91ede282c2353595ade9ba95d0de08c
Lachlan Sylvester <lachlan.sylvester@hypothetical.com.au>
2018-07-22 23:28:43 +1000

    add duration to logging

--- lib/rate_limiter/log_subscriber.rb
+++ lib/rate_limiter/log_subscriber.rb
@@ -8,7 +8,7 @@ module RateLimiter
         action = "Allowed"
         severity = :debug
       end
-      send severity, "[RateLimiter] #{action} request for #{event.payload[:identifier]} (#{event.payload[:count]}/#{event.payload[:limit]}). Expires at #{event.payload[:expires]}"
+      send severity, "[RateLimiter] #{action} request for #{event.payload[:identifier]} (#{event.payload[:count]}/#{event.payload[:limit]}). Expires at #{event.payload[:expires] }. (#{event.duration.round(1)}ms)"
     end
   end
 end
